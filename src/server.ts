ff83b36a33d3fa94e4ac48a19a78718d51ec75ad
Peter <dyumin.peter@yandex.com>
2025-04-23 22:43:14 +0300

    Work on local version

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
-import { createServer } from 'node:http';
+import { createServer as createHttpServer } from 'node:http';
+import { createServer as createHttpsServer } from 'node:https';
+import express from 'express';
 import { Server as IOServer } from 'socket.io';
+import ip from 'ip';
+import qr from 'qrcode-terminal';
+import { readFileSync } from 'node:fs';
 
 import {
   ALLOWED_ORIGINS,
   CHUNK_SIZE_BYTES,
   EVENTS,
+  IS_LOCAL,
   NODE_ENV,
   PORT,
 } from './configuration';
@@ -13,14 +19,43 @@ import gracefulShutdown from './utilities/graceful-shutdown';
 import router from './router';
 import log from './utilities/log';
 
-const serverInstance = createServer();
+const ADDRESS = ip.address();
+
+// TODO: frontend URL should not be hardcoded
+const FRONTEND_URL = `https://${ADDRESS}:3000`;
+
+const allowedOrigins = [...ALLOWED_ORIGINS];
+if (IS_LOCAL) {
+  allowedOrigins.push(FRONTEND_URL);
+}
+
+function createServerInstance(isLocal: boolean) {
+  if (!isLocal) {
+    return createHttpServer();
+  }
+
+  const app = express();
+  app.get('/', (_, response) => response.redirect(FRONTEND_URL));
+
+  qr.generate(`https://${ADDRESS}:${PORT}`, { small: true });
+
+  return createHttpsServer(
+    {
+      cert: readFileSync(`${process.cwd()}/certificates/cert.pem`),
+      key: readFileSync(`${process.cwd()}/certificates/key.pem`),
+    },
+    app,
+  );
+}
+
+const serverInstance = createServerInstance(IS_LOCAL);
 
 const io = new IOServer(
   serverInstance,
   {
     cors: {
       credentials: true,
-      origin: ALLOWED_ORIGINS,
+      origin: allowedOrigins,
     },
     maxHttpBufferSize: CHUNK_SIZE_BYTES * 2,
     pingInterval: 25000,
@@ -45,4 +80,7 @@ if (NODE_ENV === 'production') {
   );
 }
 
-serverInstance.listen(PORT, () => log(`Running on port ${PORT}`));
+serverInstance.listen(
+  PORT,
+  () => log(`Running on port ${PORT}${IS_LOCAL ? ' [LOCAL]' : ''}`),
+);
