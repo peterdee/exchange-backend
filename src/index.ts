38b67ce8d7373ceeaaaa320b74f8c69dbf5b1164
Peter <p.dyumin@tg.dunice.net>
2025-11-05 10:42:59 +0300

    Replace ip with internal-ip, re-lock

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { stat } from 'node:fs/promises';
 (async () => {
   try {
     await stat(join(process.cwd(), '.env'));
-    config();
+    config({ quiet: true });
   } catch (error) {
     if (error.code !== 'ENOENT') {
       throw error;
